be185c44d180ff57bccaaacaf0d79c4f761ad604
Chris Churas <churas.camera@gmail.com>
2020-04-01 18:04:17 -0700

    added optional arguments for hidef

--- cdhidef/cdhidefcmd.py
+++ cdhidef/cdhidefcmd.py
@@ -9,9 +9,14 @@ import uuid
 import csv
 import shutil
 
-DEFAULT_ERR_MSG = ('Did not get any clusters from HiDeF. This could be ' +
-                   'due to a network that is too connected or ' +
-                   ' the resolution parameter is too extreme\n')
+DEFAULT_ERR_MSG = ('Did not get any clusters from HiDeF. Not sure'
+                   ' what is going on\n')
+
+X_PREFIX = 'x'
+
+class Formatter(argparse.ArgumentDefaultsHelpFormatter,
+                argparse.RawDescriptionHelpFormatter):
+    pass
 
 
 def _parse_arguments(desc, args):
@@ -21,11 +26,37 @@ def _parse_arguments(desc, args):
     :param args:
     :return:
     """
-    help_fm = argparse.ArgumentDefaultsHelpFormatter
     parser = argparse.ArgumentParser(description=desc,
-                                     formatter_class=help_fm)
+                                     formatter_class=Formatter)
     parser.add_argument('input',
                         help='Edge file in tab delimited format')
+    parser.add_argument('--n', type=int,
+                        help='explore the maximum resolution '
+                             'parameter until cluster number '
+                             'is close enough to this value. '
+                             'Increase this number to get more '
+                             'smaller clusters')
+    parser.add_argument('--t', type=float, default=0.1,
+                        help='(inversed) density of sampling '
+                             'the resolution parameter; decrease '
+                             'this number to introduce more transient '
+                             'clusters (with longer running time);')
+    parser.add_argument('--k', type=int, default=5,
+                        help='a parameter to pre-filter instable '
+                             'clusters')
+    parser.add_argument('--j', type=float, default=0.75,
+                        help='a jaccard index cutoff')
+    parser.add_argument('--minres', type=float, default=0.001,
+                        help='minimum resolution parameter')
+    parser.add_argument('--maxres', type=float, default=100.0,
+                        help='maximum resolution parameter')
+    parser.add_argument('--s', type=float, default=1.0,
+                        help='a subsample parameter')
+    parser.add_argument('--ct', default=75, type=int,
+                        help='threshold in collapsing cluster')
+    parser.add_argument('--alg', default='louvain',
+                        choices=['louvain', 'leiden'],
+                        help='add the option to use leiden algorithm')
     parser.add_argument('--hidefcmd',
                         default='hidef_finder.py',
                         help='Path to hidef_finder.py command')
@@ -36,8 +67,13 @@ def _parse_arguments(desc, args):
 
 def create_tmpdir(theargs):
     """
-    :param theargs:
-    :return:
+    Creates temp directory for hidef output with
+    a unique name of format cdhidef_<UUID>
+
+    :param theargs: Holds attributes from argparse
+    :type theargs: `:py:class:`argparse.Namespace`
+    :return: Path to temp directory
+    :rtype: str
     """
     tmpdir = os.path.join(theargs.tempdir, 'cdhidef_' + str(uuid.uuid4()))
     os.makedirs(tmpdir, mode=0o755)
@@ -46,9 +82,11 @@ def create_tmpdir(theargs):
 
 def run_hidef_cmd(cmd):
     """
-    Runs hidef command
+    Runs hidef command as a command line process
     :param cmd_to_run: command to run as list
-    :return:
+    :type cmd_to_run: list
+    :return: (return code, standard out, standard error)
+    :rtype: tuple
     """
     p = subprocess.Popen(cmd,
                          stdout=subprocess.PIPE,
@@ -61,9 +99,18 @@ def run_hidef_cmd(cmd):
 
 def get_max_node_id(nodes_file):
     """
+    Examines the 'nodes_file' passed in and finds the value of
+    highest node id.
+
+    It is assumed the 'nodes_file' a tab delimited
+    file of format:
+
+    <CLUSTER NAME> <# NODES> <SPACE DELIMITED NODE IDS> <SCORE>
 
     :param nodes_file:
-    :return:
+    :type nodes_file: Path to to nodes file from hidef output
+    :return: highest node id found
+    :rtype: int
     """
     maxval = None
     with open(nodes_file, 'r') as csvfile:
@@ -81,23 +128,47 @@ def get_max_node_id(nodes_file):
 
 def write_members_for_row(out_stream, row, cur_node_id):
     """
+    Given a row from nodes file from hidef output the members
+    of the clusters by parsing the <SPACE DELIMITED NODE IDS>
+    as mentioned in :py:func:`#get_max_node_id` description.
+
+    The output is written to `out_stream` for each node id
+    in format:
+
+    <cur_node_id>,<node id>,c-m;
 
     :param out_stream:
-    :param row:
-    :param cur_node_id:
-    :return:
+    :type out_stream: file like object
+    :param row: Should be a line from hidef nodes file parsed
+                by :py:func:`csv.reader`
+    :type row: iterator
+    :param cur_node_id: id of cluster that contains the nodes
+    :type cur_node_id: int
+    :return: None
     """
     for node in row[2].split(' '):
         out_stream.write(str(cur_node_id) + ',' +
                          node + ',c-m;')
 
+
 def update_cluster_node_map(cluster_node_map, cluster, max_node_id):
     """
+    Updates 'cluster_node_map' which is in format of
 
-    :param cluster_node_map:
-    :param cluster:
-    :param max_node_id:
-    :return:
+    <cluster name> => <node id>
+
+    by adding 'cluster' to 'cluster_node_map' if it does not
+    exist
+
+    :param cluster_node_map: map of cluster names to node ids
+    :type cluster_node_map: dict
+    :param cluster: name of cluster
+    :type cluster: str
+    :param max_node_id: current max node id
+    :type max_node_id: int
+    :return: (new 'max_node_id' if 'cluster' was added otherwise 'max_node_id',
+              id corresponding to 'cluster' found in 'cluster_node_map')
+    :rtype: tuple
     """
     if cluster not in cluster_node_map:
         max_node_id += 1
@@ -110,10 +181,28 @@ def update_cluster_node_map(cluster_node_map, cluster, max_node_id):
 
 def write_communities(out_stream, edge_file, cluster_node_map):
     """
+    Writes out links between clusters in COMMUNITYDETECTRESULT format
+    as noted in :py:func:`#convert_hidef_output_to_cdaps`
 
-    :param out_stream:
-    :param edgefile:
-    :return:
+    using hidef edge file set in 'edge_file' that is expected to
+    be in this tab delimited format:
+
+    <SOURCE CLUSTER> <TARGET CLUSTER> <default>
+
+    This function converts the <SOURCE CLUSTER> <TARGET CLUSTER>
+    to new node ids (leveraging 'cluster_node_map')
+
+    and writes the following output:
+
+    <SOURCE CLUSTER NODE ID>,<TARGET CLUSTER NODE ID>,c-c;
+
+    to the 'out_stream'
+
+    :param out_stream: output stream
+    :type out_stream: file like object
+    :param edge_file: path to hidef edges file
+    :type edge_file: str
+    :return: None
     """
     with open(edge_file, 'r') as csvfile:
         linereader = csv.reader(csvfile, delimiter='\t')
@@ -122,20 +211,29 @@ def write_communities(out_stream, edge_file, cluster_node_map):
                              str(cluster_node_map[row[1]]) + ',c-c;')
 
 
-def convert_hidef_output_to_cdaps(out_stream, tmpdir):
+def convert_hidef_output_to_cdaps(out_stream, tempdir):
     """
-    Looks for x.nodes and x.edges in `tmpdir` directory
+    Looks for x.nodes and x.edges in `tempdir` directory
     to generate output in COMMUNITYDETECTRESULT format:
     https://github.com/idekerlab/communitydetection-rest-server/wiki/COMMUNITYDETECTRESULT-format
 
-    which is
+    This method leverages
+
+    :py:func:`#write_members_for_row`
+
+    and
+
+    :py:func:`#write_communities`
+
+    to write output
+
     :param out_stream: output stream to write results
     :type out_stream: file like object
-    :param tmpdir:
-    :type tmpdir: str
-    :return:
+    :param tempdir:
+    :type tempdir: str
+    :return: None
     """
-    nodefile = os.path.join(tmpdir, 'x.nodes')
+    nodefile = os.path.join(tempdir, X_PREFIX + '.nodes')
     max_node_id = get_max_node_id(nodefile)
     cluster_node_map = {}
     with open(nodefile, 'r') as csvfile:
@@ -146,50 +244,87 @@ def convert_hidef_output_to_cdaps(out_stream, tmpdir):
                                                                max_node_id)
             write_members_for_row(out_stream, row,
                                   cur_node_id)
-    edge_file = os.path.join(tmpdir, 'x.edges')
+    edge_file = os.path.join(tempdir, X_PREFIX + '.edges')
     write_communities(out_stream, edge_file, cluster_node_map)
     out_stream.write('\n')
     return None
 
 
-def run_hidef(theargs):
+def build_optional_arguments(theargs):
     """
+    Builds list of optional arguments and values
 
-    :param theargs:
-    :return:
+    :param theargs: Holds attributes from argparse
+    :type theargs: `:py:class:`argparse.Namespace`
+    :return: arguments for hidef in list of str objects
+    :rtype: list
+    """
+    cmdargs = []
+    if theargs.n is not None:
+        cmdargs.extend(['--n', str(theargs.n)])
+
+    cmdargs.extend(['--t', str(theargs.t)])
+    cmdargs.extend(['--k', str(theargs.k)])
+    cmdargs.extend(['--j', str(theargs.j)])
+    cmdargs.extend(['--minres', str(theargs.minres)])
+    cmdargs.extend(['--maxres', str(theargs.maxres)])
+    cmdargs.extend(['--s', str(theargs.s)])
+    cmdargs.extend(['--ct', str(theargs.ct)])
+    cmdargs.extend(['--alg', theargs.alg])
+    return cmdargs
+
+
+def run_hidef(theargs, out_stream=sys.stdout,
+              err_stream=sys.stderr):
+    """
+    Runs hidef command line program and then converts
+    output to CDAPS compatible format that is output to
+    standard out.
+
+    :param theargs: Holds attributes from argparse
+    :type theargs: `:py:class:`argparse.Namespace`
+    :param out_stream: stream for standard output
+    :type out_stream: file like object
+    :param err_stream: stream for standard error output
+    :type err_stream: file like object
+    :return: 0 upon success otherwise error
+    :rtype: int
     """
     cmdargs = []
     cmdargs.extend([theargs.hidefcmd, '--g', theargs.input,
                     '--skipclug', '--skipgml'])
+    cmdargs.extend(build_optional_arguments(theargs))
+
     if theargs.input is None or not os.path.isfile(theargs.input):
-        sys.stderr.write(str(theargs.input) + ' is not a file')
+        err_stream.write(str(theargs.input) + ' is not a file')
         return 3
 
     if os.path.getsize(theargs.input) == 0:
-        sys.stderr.write(str(theargs.input) + ' is an empty file')
+        err_stream.write(str(theargs.input) + ' is an empty file')
         return 4
 
     tmpdir = create_tmpdir(theargs)
     try:
-        outval = os.path.join(tmpdir, 'x')
+        outval = os.path.join(tmpdir, X_PREFIX)
         cmdargs.extend(['--o', outval])
         sys.stderr.write('Running ' + str(' '.join(cmdargs)) + '\n')
         sys.stderr.flush()
         cmdecode, cmdout, cmderr = run_hidef_cmd(cmdargs)
 
         if cmdecode != 0:
-            sys.stderr.write('Command failed with non-zero exit code: ' +
+            err_stream.write('Command failed with non-zero exit code: ' +
                              str(cmdecode) + ' : ' + str(cmderr) + '\n')
             return 1
 
         if len(cmdout) > 0:
-            sys.stderr.write('Output from cmd: ' + str(cmdout) + '\n')
+            err_stream.write('Output from cmd: ' + str(cmdout) + '\n')
 
         if len(cmderr) > 0:
-            sys.stderr.write('Error output from cmd: ' + str(cmderr) + '\n')
+            err_stream.write('Error output from cmd: ' + str(cmderr) + '\n')
 
-        convert_hidef_output_to_cdaps(sys.stdout, tmpdir)
-        sys.stdout.flush()
+        convert_hidef_output_to_cdaps(out_stream, tmpdir)
+        err_stream.flush()
+        out_stream.flush()
         return 0
     finally:
         shutil.rmtree(tmpdir)
@@ -206,13 +341,9 @@ def main(args):
     Runs HiDeF on command line, sending output to standard
     out 
     """
-
     theargs = _parse_arguments(desc, args[1:])
-
     try:
-
-        return run_hidef(theargs)
-
+        return run_hidef(theargs, sys.stdout, sys.stderr)
     except Exception as e:
         sys.stderr.write('\n\nCaught exception: ' + str(e))
         traceback.print_exc()
--- tests/test_cdhidef.py
+++ tests/test_cdhidef.py
@@ -31,10 +31,120 @@ class TestCdhidef(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_parse_args(self):
+    def test_parse_args_all_defaults(self):
         myargs = ['inputarg']
         res = cdhidefcmd._parse_arguments('desc', myargs)
         self.assertEqual('inputarg', res.input)
+        self.assertEqual(None, res.n)
+        self.assertEqual(0.1, res.t)
+        self.assertEqual(5, res.k)
+        self.assertEqual(0.75, res.j)
+        self.assertEqual(0.001, res.minres)
+        self.assertEqual(100.0, res.maxres)
+        self.assertEqual(1.0, res.s)
+        self.assertEqual(75, res.ct)
+        self.assertEqual('louvain', res.alg)
+        self.assertEqual('hidef_finder.py', res.hidefcmd)
+        self.assertEqual('/tmp', res.tempdir)
+
+    def test_parse_args_custom_params(self):
+        myargs = ['i2',
+                  '--n', '1',
+                  '--t', '0.2',
+                  '--k', '3',
+                  '--j', '0.4',
+                  '--minres', '0.5',
+                  '--maxres', '0.6',
+                  '--s', '0.7',
+                  '--ct', '8',
+                  '--alg', 'leiden',
+                  '--hidefcmd', 'foo',
+                  '--tempdir', 'yo']
+        res = cdhidefcmd._parse_arguments('desc', myargs)
+        self.assertEqual('i2', res.input)
+        self.assertEqual(1, res.n)
+        self.assertEqual(0.2, res.t)
+        self.assertEqual(3, res.k)
+        self.assertEqual(0.4, res.j)
+        self.assertEqual(0.5, res.minres)
+        self.assertEqual(0.6, res.maxres)
+        self.assertEqual(0.7, res.s)
+        self.assertEqual(8, res.ct)
+        self.assertEqual('leiden', res.alg)
+        self.assertEqual('foo', res.hidefcmd)
+        self.assertEqual('yo', res.tempdir)
+
+    def test_build_optional_arguments_with_n(self):
+        myargs = ['i2',
+                  '--n', '1',
+                  '--t', '0.2',
+                  '--k', '3',
+                  '--j', '0.4',
+                  '--minres', '0.5',
+                  '--maxres', '0.6',
+                  '--s', '0.7',
+                  '--ct', '8',
+                  '--alg', 'leiden',
+                  '--hidefcmd', 'foo',
+                  '--tempdir', 'yo']
+        res = cdhidefcmd._parse_arguments('desc', myargs)
+        optargs = cdhidefcmd.build_optional_arguments(res)
+
+        self.assertEqual('--n', optargs[0])
+        self.assertEqual('1', optargs[1])
+
+        self.assertEqual('--t', optargs[2])
+        self.assertEqual('0.2', optargs[3])
+
+        self.assertEqual('--k', optargs[4])
+        self.assertEqual('3', optargs[5])
+
+        self.assertEqual('--j', optargs[6])
+        self.assertEqual('0.4', optargs[7])
+
+        self.assertEqual('--minres', optargs[8])
+        self.assertEqual('0.5', optargs[9])
+
+        self.assertEqual('--maxres', optargs[10])
+        self.assertEqual('0.6', optargs[11])
+
+        self.assertEqual('--s', optargs[12])
+        self.assertEqual('0.7', optargs[13])
+
+        self.assertEqual('--ct', optargs[14])
+        self.assertEqual('8', optargs[15])
+
+        self.assertEqual('--alg', optargs[16])
+        self.assertEqual('leiden', optargs[17])
+
+    def test_build_optional_arguments(self):
+        myargs = ['i2']
+        res = cdhidefcmd._parse_arguments('desc', myargs)
+        optargs = cdhidefcmd.build_optional_arguments(res)
+        self.assertEqual('--t', optargs[0])
+        self.assertEqual('0.1', optargs[1])
+
+        self.assertEqual('--k', optargs[2])
+        self.assertEqual('5', optargs[3])
+
+        self.assertEqual('--j', optargs[4])
+        self.assertEqual('0.75', optargs[5])
+
+        self.assertEqual('--minres', optargs[6])
+        self.assertEqual('0.001', optargs[7])
+
+        self.assertEqual('--maxres', optargs[8])
+        self.assertEqual('100.0', optargs[9])
+
+        self.assertEqual('--s', optargs[10])
+        self.assertEqual('1.0', optargs[11])
+
+        self.assertEqual('--ct', optargs[12])
+        self.assertEqual('75', optargs[13])
+
+        self.assertEqual('--alg', optargs[14])
+        self.assertEqual('louvain', optargs[15])
+
 
     def test_run_hidef_no_file(self):
         temp_dir = tempfile.mkdtemp()
